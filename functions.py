f99bb61eca4cfb18f31bcade201d1ef1bd861ab5
pseregiet <patryk.seregiet@gmail.com>
2025-08-14 20:18:46 +0200

    more work

--- functions.py
+++ functions.py
@@ -6,6 +6,19 @@ import json
 import re
 from collections import namedtuple
 
+g_offset = 0x800C9578
+
+#based on https://github.com/Vatuu/silent-hill-decomp/blob/master/tools/silentassets/extract.py
+def xorBodyprog(data):
+    outArray = memoryview(data).cast("I") # uint32_t
+    seed = 0
+
+    for i, value in enumerate(outArray):
+        seed = (seed + 0x01309125) & 0xffffffff
+        seed = (seed * 0x03a452f7) & 0xffffffff
+        outArray[i] ^= seed
+
+
 def read_uint32_le(buf, offset):
     return struct.unpack_from('<I', buf, offset)[0]
 
@@ -27,11 +40,22 @@ def find_blob_in_bin(blob, data, name):
     return r
 
 def nice_encoding(line):
-    pattern = re.compile(r'(~[CLS]\d|~[MDHET])')
-    jpattern= re.compile(r'(~J\d\(\d\.\d\))')
-    line = line.replace(' ', '').replace('_', ' ').replace('\n', '').replace('\t', '').replace('~N', '\n')
-    line = pattern.sub(r' \1 ', line)
-    return jpattern.sub(r' \1 ', line).strip()
+    pattern = re.compile(r'(~[CLS]\d|~[MDHETN])')
+    jpattern= re.compile(r'(~J\d\(\d+\.\d+\))')
+    line = line.replace(' ', '').replace('\n', '').replace('\t', '')
+    line = line.replace('_', ' ')
+    line = pattern.sub(r'{\1}', line)
+    return jpattern.sub(r'{\1}', line).strip()
+
+def encode_local_text(line):
+    line = line.replace('ł', ';').replace('ó', '*').replace('ę', '>').replace('ą', '^')
+    line = line.replace('ż', '=').replace('ć', '<').replace('ń', '+').replace('ś', '/').replace('ź', 'Q')
+    return line
+
+def nice_decode(line):
+    line = line.replace(' ', '_').replace('{', ' ').replace('}', ' ')
+    line = encode_local_text(line)
+    return line.encode('ascii') + b'\x00'
 
 def revert(filename):
     translated_lines = read_translated_lines(filename + ".tr.txt")
@@ -43,67 +67,56 @@ def revert(filename):
 
 MapInfo = namedtuple("MapInfo", ["sector_start", "block_size", "filename"])
 MapInfos = [
-    MapInfo(0x092af,  422,  "map0_s00.bin"),
-    #MapInfo(0x092e5,  401,  "map0_s01.bin"),
-    #MapInfo(0x09317,  160,  "map0_s02.bin"),
-    #MapInfo(0x0932b,  381,  "map1_s00.bin"),
-    #MapInfo(0x0935b,  349,  "map1_s01.bin"),
-    #MapInfo(0x09387,  454,  "map1_s02.bin"),
-    #MapInfo(0x093c0,  463,  "map1_s03.bin"),
-    #MapInfo(0x093fa,   75,  "map1_s04.bin"),
-    #MapInfo(0x09404,  242,  "map1_s05.bin"),
-    #MapInfo(0x09423,  284,  "map1_s06.bin"),
-    #MapInfo(0x09447,  708,  "map2_s00.bin"),
-    #MapInfo(0x094a0,  131,  "map2_s01.bin"),
-    #MapInfo(0x094b1,  631,  "map2_s02.bin"),
-    #MapInfo(0x09500,   74,  "map2_s03.bin"),
-    #MapInfo(0x0950a,   95,  "map2_s04.bin"),
-    #MapInfo(0x09516,  201,  "map3_s00.bin"),
-    #MapInfo(0x09530,  243,  "map3_s01.bin"),
-    #MapInfo(0x0954f,  156,  "map3_s02.bin"),
-    MapInfo(0x09563,  240,  "map3_s03.bin"),
-    #MapInfo(0x09581,  217,  "map3_s04.bin"),
-    #MapInfo(0x0959d,  320,  "map3_s05.bin"),
-    #MapInfo(0x095c5,  203,  "map3_s06.bin"),
-    #MapInfo(0x095df,   64,  "map4_s00.bin"),
-    #MapInfo(0x095e7,  236,  "map4_s01.bin"),
-    #MapInfo(0x09605,  639,  "map4_s02.bin"),
-    #MapInfo(0x09655,  373,  "map4_s03.bin"),
-    #MapInfo(0x09684,  218,  "map4_s04.bin"),
-    #MapInfo(0x096a0,  293,  "map4_s05.bin"),
-    #MapInfo(0x096c5,   64,  "map4_s06.bin"),
-    #MapInfo(0x096cd,  335,  "map5_s00.bin"),
-    #MapInfo(0x096f7,  682,  "map5_s01.bin"),
-    #MapInfo(0x0974d,  277,  "map5_s02.bin"),
-    #MapInfo(0x09770,  220,  "map5_s03.bin"),
-    #MapInfo(0x0978c,  684,  "map6_s00.bin"),
-    #MapInfo(0x097e2,  193,  "map6_s01.bin"),
-    #MapInfo(0x097fb,  185,  "map6_s02.bin"),
-    #MapInfo(0x09813,  363,  "map6_s03.bin"),
-    #MapInfo(0x09841,  586,  "map6_s04.bin"),
-    #MapInfo(0x0988b,   65,  "map6_s05.bin"),
-    #MapInfo(0x09894,  175,  "map7_s00.bin"),
-    #MapInfo(0x098aa,  415,  "map7_s01.bin"),
-    #MapInfo(0x098de,  559,  "map7_s02.bin"),
-    #MapInfo(0x09924,  698,  "map7_s03.bin"),
+    MapInfo(0x092af,  422,  "map0_s00"),
+    MapInfo(0x092e4,  401,  "map0_s01"),
+    MapInfo(0x09317,  160,  "map0_s02"),
+    MapInfo(0x0932b,  381,  "map1_s00"),
+    MapInfo(0x0935b,  349,  "map1_s01"),
+    MapInfo(0x09387,  454,  "map1_s02"),
+    MapInfo(0x093c0,  463,  "map1_s03"),
+    #MapInfo(0x093fa,   75,  "map1_s04"),
+    MapInfo(0x09404,  242,  "map1_s05"),
+    MapInfo(0x09423,  284,  "map1_s06"),
+    MapInfo(0x09447,  708,  "map2_s00"),
+    MapInfo(0x094a0,  131,  "map2_s01"),
+    MapInfo(0x094b1,  631,  "map2_s02"),
+    #MapInfo(0x09500,   74,  "map2_s03"),
+    MapInfo(0x0950a,   95,  "map2_s04"),
+    MapInfo(0x09516,  201,  "map3_s00"),
+    MapInfo(0x09530,  243,  "map3_s01"),
+    MapInfo(0x0954f,  156,  "map3_s02"),
+    MapInfo(0x09563,  240,  "map3_s03"),
+    MapInfo(0x09581,  217,  "map3_s04"),
+    MapInfo(0x0959d,  320,  "map3_s05"),
+    MapInfo(0x095c5,  203,  "map3_s06"),
+    #MapInfo(0x095df,   64,  "map4_s00"),
+    MapInfo(0x095e7,  236,  "map4_s01"),
+    MapInfo(0x09605,  639,  "map4_s02"),
+    MapInfo(0x09655,  373,  "map4_s03"),
+    MapInfo(0x09684,  218,  "map4_s04"),
+    MapInfo(0x096a0,  293,  "map4_s05"),
+    #MapInfo(0x096c5,   64,  "map4_s06"),
+    MapInfo(0x096cd,  335,  "map5_s00"),
+    MapInfo(0x096f7,  682,  "map5_s01"),
+    MapInfo(0x0974d,  277,  "map5_s02"),
+    MapInfo(0x09770,  220,  "map5_s03"),
+    MapInfo(0x0978c,  684,  "map6_s00"),
+    MapInfo(0x097e2,  193,  "map6_s01"),
+    MapInfo(0x097fb,  185,  "map6_s02"),
+    MapInfo(0x09813,  363,  "map6_s03"),
+    MapInfo(0x09841,  586,  "map6_s04"),
+    #MapInfo(0x0988b,   65,  "map6_s05"),
+    MapInfo(0x09894,  175,  "map7_s00"),
+    MapInfo(0x098aa,  415,  "map7_s01"),
+    MapInfo(0x098de,  559,  "map7_s02"),
+    MapInfo(0x09924,  698,  "map7_s03"),
 ]
 
+BodyProg = MapInfo(0x000cf, 2635, "bodyprog")
 
-
-overlays = [ "map0_s00.asciz", "map0_s01.asciz", "map0_s02.asciz", "map1_s00.asciz",
-             "map1_s01.asciz", "map1_s02.asciz", "map1_s03.asciz", "map1_s04.asciz",
-             "map1_s05.asciz", "map1_s06.asciz", "map2_s00.asciz", "map2_s01.asciz",
-             "map2_s02.asciz", "map2_s03.asciz", "map2_s04.asciz", "map3_s00.asciz",
-             "map3_s01.asciz", "map3_s02.asciz", "map3_s03.asciz", "map3_s04.asciz",
-             "map3_s05.asciz", "map3_s06.asciz", "map4_s00.asciz", "map4_s01.asciz",
-             "map4_s02.asciz", "map4_s04.asciz", "map4_s05.asciz", "map4_s06.asciz",
-             "map5_s00.asciz", "map5_s01.asciz", "map5_s02.asciz", "map5_s03.asciz",
-             "map6_s00.asciz", "map6_s01.asciz", "map6_s02.asciz", "map6_s03.asciz",
-             "map6_s04.asciz", "map6_s05.asciz", "map7_s00.asciz", "map7_s01.asciz",
-             "map7_s02.asciz", "map7_s03.asciz" ]
-
-#for ov in overlays:
-#    revert(ov)
+def extract_bodyprog(silent):
+    start = (BodyProg.sector_start - 0x40) * 0x800
+    size  = BodyProg.
 
 def extract_map_bin(silent, mi):
     start = (mi.sector_start - 0x40) * 0x800
@@ -112,43 +125,76 @@ def extract_map_bin(silent, mi):
     return silent[start: start+size]
 
 def extract_map_messages(silent, mi):
-    offset = 0x800C9578
     mapdata = extract_map_bin(silent, mi)
 
-    ptroffset = read_uint32_le(mapdata, 0x34) - offset
-    endoffset = read_uint32_le(mapdata, 0x28) - offset
-    msgcount = int((endoffset - ptroffset) / 4)
-    txt_last_byte = read_uint32_le(mapdata, ptroffset) - offset
+    ptroffset = read_uint32_le(mapdata, 0x34) - g_offset
+    endoffset = read_uint32_le(mapdata, 0x28) - g_offset
+
+    i = 0
+    msgs = []
+    msgptr = ptroffset
+
+    while True:
+        if msgptr == endoffset:
+            break
+
+        msg1 = read_uint32_le(mapdata, msgptr)
+        if msg1 == 0:
+            break
+
+        msg1 -= g_offset
+        msg1_str = read_c_string(mapdata, msg1)
+        msgs.append(nice_encoding(msg1_str))
+        msgptr += 4
+        i += 1
+
+    txt_last_byte = read_uint32_le(mapdata, ptroffset) - g_offset
     txt_last_byte += len(read_c_string(mapdata, txt_last_byte))
-    txtoffset = read_uint32_le(mapdata, ptroffset + ((msgcount-1)*4)) - offset
+    txtoffset = read_uint32_le(mapdata, ptroffset + ((i-1)*4)) - g_offset
     txtlen = txt_last_byte - txtoffset
 
     output = {
             "txt-offset": f"0x{txtoffset:X}",
             "ptr-offset": f"0x{ptroffset:X}",
             "txt-size"  : f"0x{txtlen:X}",
-            "ptr-size"  : f"0x{msgcount:X}", 
+            "ptr-size"  : f"0x{i:X}", 
     }
-
-    i = 0
-    msgs = []
-    while True:
-        if ptroffset + i*4 == endoffset:
-            break
-
-        msg1 = read_uint32_le(mapdata, ptroffset + i*4)
-        msg1 -= offset
-        print(f"{i}: {msg1:X}")
-
-        i += 1
-        msg1_str = read_c_string(mapdata, msg1)
-        msgs.append(nice_encoding(msg1_str))
-        #print(msg1_str)
 
     output["messages"] = msgs
     with open("messages/" + mi.filename + ".json", 'w') as f:
-        json.dump(output, sys.stdout, indent=4)
+        #f = sys.stdout
+        json.dump(output, f, indent=4)
 
+def patch_blob(dstblob, srcblob, offset):
+    bloblen = len(srcblob)
+    dstblob[offset: offset + bloblen] = srcblob
+
+def patch_pointer(dstblob, pointer, offset):
+    srcblob = struct.pack('<I', pointer)
+    patch_blob(dstblob, srcblob, offset)
+
+def patch_map(silent, mi):
+    with open("messages/" + mi.filename + ".json", 'r', encoding="utf-8") as f:
+        data = json.load(f)
+
+    mapdata = extract_map_bin(silent, mi)
+    txt_offset = int(data['txt-offset'], 16)
+    ptr_offset = int(data['ptr-offset'], 16)
+    max_txt_size = int(data['txt-size'], 16)
+    max_txt_size += txt_offset
+    total_size = 0
+    for line in data['messages']:
+        txt_pointer = g_offset + txt_offset
+        game_encoded_line = nice_decode(line)
+        patch_pointer(mapdata, txt_pointer, ptr_offset)
+        patch_blob(mapdata, game_encoded_line, txt_offset)
+        txt_offset += len(game_encoded_line)
+        ptr_offset += 4
+        if txt_offset > max_txt_size:
+            raise MemoryError("Not enough room for the new text")
+
+    start = (mi.sector_start - 0x40) * 0x800
+    patch_blob(silent, mapdata, start)
 
 def main():
     silent_path = sys.argv[1]
@@ -156,8 +202,13 @@ def main():
     with open(silent_path, 'rb') as f:
         silent = bytearray(f.read())
 
-    for mi in MapInfos:
-        extract_map_messages(silent, mi)
+    #for mi in MapInfos:
+        #patch_map(silent, mi)
+        #extract_map_messages(silent, mi)
 
+    patch_map(silent, MapInfos[0])
+
+    with open(silent_path+".new", 'wb') as f:
+        f.write(silent)
 
 main()
